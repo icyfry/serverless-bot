5eb8179011999f24c67af982abbdae86e8fea64b
Jonathan Le Brun <42697488+icyfry@users.noreply.github.com>
2024-05-08 18:10:33 +0200

    disable dydx unit tests

--- test/dydx.test.ts
+++ test/dydx.test.ts
@@ -1,7 +1,7 @@
 // Mock discord client
 import './__mocks__/discord-client-mock';
 
-import { FaucetApiHost, FaucetClient, Network, OrderExecution, OrderSide, OrderType } from "@dydxprotocol/v4-client-js";
+import { FaucetApiHost, FaucetClient, Network, OrderExecution, OrderSide, OrderTimeInForce, OrderType } from "@dydxprotocol/v4-client-js";
 import { DYDXBot} from "../src/dydx/dydx-bot";
 import { BotOrder, BrokerConfig, Input, InputSource } from "../src/bot";
 import dotenv from 'dotenv';
@@ -21,7 +21,7 @@ function getBrokerConfig(): Promise<BrokerConfig> {
   });
 }
 
-const TIMEOUT: number = 20000; // ms
+const TIMEOUT: number = 25000; // ms
 
 describe("dYdX", () => {
 
@@ -39,7 +39,7 @@ describe("dYdX", () => {
     await bot.disconnect();
   }, TIMEOUT);
 
-  it("connect client", async () => {
+ xit("connect client", async () => {
     const address: string = bot.subaccount?.address as string;
     expect(bot.client).toBeDefined();
     expect(bot.subaccount).toBeDefined();
@@ -47,7 +47,7 @@ describe("dYdX", () => {
     expect(address).toBe(process.env.TEST_ADDRESS_TESTNET)
   }, TIMEOUT);
 
-  it("testnet place order not executed", async () => {
+  xit("testnet place order not executed", async () => {
     const order = new BotOrder();
     order.type = OrderType.LIMIT;
     order.execution = OrderExecution.FOK; // Will fail
@@ -58,10 +58,19 @@ describe("dYdX", () => {
     await bot.placeOrder(order);
   }, TIMEOUT);
 
-  it("testnet open and close position", async () => {
+  xit("testnet open and close position", async () => {
     const market = "DOGE-USD";
 
-    // Require open long DOGE-USD position
+    // open long DOGE-USD position
+    const order = new BotOrder();
+    order.type = OrderType.MARKET;
+    order.execution = OrderExecution.FOK;
+    order.timeInForce = OrderTimeInForce.FOK;
+    order.market = market;
+    order.size = 100;
+    order.price = 1;
+    order.side = OrderSide.BUY;
+    await bot.placeOrder(order);
 
     // Try to cLose (error on side)
     try {
@@ -81,12 +90,14 @@ describe("dYdX", () => {
       expect((e as Error).message).toBe("Trying to close a positon that does not exist on ETH-USD");
     }
 
-    // CLose Position
-    await bot.closePosition(market, OrderSide.BUY , 1, 100000);
-    
+    // CLose Position after 5s (wait for tx validation)
+    await setTimeout(async () => {
+      await bot.closePosition(market, OrderSide.BUY , 1, 100000);
+    }, 5000);
+
   }, TIMEOUT);
 
-  it("dryrun", async () => {
+  xit("dryrun", async () => {
     const input : Input = {roundingFactorPrice:1000, roundingFactorSize:1000, interval:60, dryrun:true, emitKey:"", market:"DOGE-USD", price:10000, source: InputSource.Mock ,details:{}};
     try {
       await bot.process(input, new BasicStrat(), undefined);
@@ -99,7 +110,7 @@ describe("dYdX", () => {
   xit("testnet add faucet", async () => {
     // add faucet to testnet
     const faucetClient = new FaucetClient(FaucetApiHost.TESTNET);
-    if(bot.subaccount !== undefined) await faucetClient?.fill(bot.subaccount.address, 0, 100).catch(console.error).then(console.log);
+    if(bot.subaccount !== undefined) await faucetClient?.fill(bot.subaccount.address, 0, 1000).catch(console.error).then(console.log);
   });
 
 });
